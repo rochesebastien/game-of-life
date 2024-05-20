b09f05101b3bdc3cb34ec861bd26e75e05c55ea9
rochesebastien <sebastienroche230@gmail.com>
2024-05-21 01:10:50 +0200

    feat(controls): game controls front done

--- src/components/GameControls/GameControls.jsx
+++ src/components/GameControls/GameControls.jsx
@@ -4,20 +4,26 @@ import './GameControls.css';
 function GameControls({ running, setRunning, resetGrid, setSpeed }) {
     return (
         <div className="game_controls">
-            <button onClick={() => setRunning(!running)}>
-                {running ? 'Pause' : 'Start'}
-            </button>
-            <button onClick={resetGrid}>Reset</button>
-            {/* <label>
-                Speed:
-                <input
+
+            <span onClick={() => setRunning(!running)}>
+                {running ? 
+                    <img src="/icons/pause.svg" alt="Pause"/> : 
+                    <img src="/icons/play.svg" alt="Play"/>
+                    }
+            </span>
+            <span onClick={resetGrid}>
+                <img src="/icons/reset.svg" alt="Play"/>
+            </span>
+            <span className="speed">
+                <img src="/icons/gauge.svg" alt="Play"/>
+                <input 
                     type="range"
                     min="100"
                     max="1000"
                     step="100"
-                    onChange={(e) => setSpeed(Number(e.target.value))}
+                    // onChange={(e) => setSpeed(Number(e.target.value))}
                 />
-            </label> */}
+            </span>
         </div>
     );
 }
